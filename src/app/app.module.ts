7015e44ba24fa18e7620e47eeb3dd4b0a20ca811
Isskoo <paillard.leo.lp@gmail.com>
2020-03-24 21:42:44 +0100

    Add material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import {AppRoutingModule} from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './angular-material.module';
+import { NavComponent } from './nav/nav.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [AppComponent, GameComponent, HomeComponent],
+  declarations: [AppComponent, GameComponent, HomeComponent, NavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FlexLayoutModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
