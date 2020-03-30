cdf51de419c3fc81ee8499d04fc9948dd1d6c637
Isskoo <paillard.leo.lp@gmail.com>
2020-03-31 01:56:48 +0200

    Ajout de la liste des joueurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {GameComponent} from './game/game.component';
 import {HomeComponent} from './home/home.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {PlayersComponent} from './players/players/players.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'game', component: GameComponent},
-  {path: '**', component: PageNotFoundComponent},
+  // {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
@@ -10,11 +10,23 @@ import {AngularMaterialModule} from './angular-material.module';
 import { NavComponent } from './nav/nav.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {AuthModule} from './auth/auth.module';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {PlayersModule} from './players/players.module';
+import {AuthService} from './shared/auth.service';
+import {HttpErrorInterceptorService} from './shared/http-error-interceptor.service';
+import {TokenInterceptorService} from './shared/token-interceptor.service';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, GameComponent, HomeComponent, NavComponent, PageNotFoundComponent],
+  declarations: [
+    AppComponent,
+    GameComponent,
+    HomeComponent,
+    NavComponent,
+    PageNotFoundComponent,
+    DialogComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,7 +35,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     FlexLayoutModule,
     AuthModule,
     HttpClientModule,
-  ],
+    PlayersModule,
+    ],
+  providers: [AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptorService, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true},
+    [{provide: LOCALE_ID, useValue: 'fr-FR'}]],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
